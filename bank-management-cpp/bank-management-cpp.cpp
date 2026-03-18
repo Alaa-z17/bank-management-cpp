4b27e3f54538b45ef386a688fd425e7e1930e1ec
Alaa Alkatshah <alaaalkatshah@gmail.com>
2026-03-18 14:22:45 +0300

    feat: add string utility convert and file functions

--- bank-management-cpp/bank-management-cpp.cpp
+++ bank-management-cpp/bank-management-cpp.cpp
@@ -84,6 +84,177 @@ const string ClientsFileName = "Clients.txt";
 const string UsersFileName = "Users.txt";
 stUser CurrentUser;
 
+// =====================
+// Forward Declarations
+// =====================
+
+void ShowMainMenu();
+void ShowTransactionsMenu();
+void ShowManageUsersMenu();
+bool CheckAccessPermission(enMainMenuPermissions Permission);
+void Login();
+void AddDataLineToFile(string FileName, string stDataLine);
+
+// =====================
+// String Utility Functions
+// =====================
+
+vector<string> SplitString(string S1, string Delim)
+{
+    vector<string> vString;
+    string::size_type pos = 0;
+    string sWord;
+
+    while ((pos = S1.find(Delim)) != string::npos)
+    {
+        sWord = S1.substr(0, pos);
+        if (sWord != "")
+            vString.push_back(sWord);
+        S1.erase(0, pos + Delim.length());
+    }
+    if (S1 != "")
+        vString.push_back(S1);
+
+    return vString;
+}
+
+// =====================
+// Convert Functions
+// =====================
+
+stClient ConvertLineToClientRecord(string Line,
+    string Separator = "#//#")
+{
+    stClient Client;
+    vector<string> vData = SplitString(Line, Separator);
+
+    Client.AccountNumber = vData[0];
+    Client.PinCode = vData[1];
+    Client.Name = vData[2];
+    Client.Phone = vData[3];
+    Client.AccountBalance = stod(vData[4]);
+
+    return Client;
+}
+
+stUser ConvertLineToUserRecord(string Line,
+    string Separator = "#//#")
+{
+    stUser User;
+    vector<string> vData = SplitString(Line, Separator);
+
+    User.UserName = vData[0];
+    User.Password = vData[1];
+    User.Permissions = stoi(vData[2]);
+
+    return User;
+}
+
+string ConvertClientRecordToLine(stClient Client,
+    string Separator = "#//#")
+{
+    string Line = "";
+    Line += Client.AccountNumber + Separator;
+    Line += Client.PinCode + Separator;
+    Line += Client.Name + Separator;
+    Line += Client.Phone + Separator;
+    Line += to_string(Client.AccountBalance);
+    return Line;
+}
+
+string ConvertUserRecordToLine(stUser User,
+    string Separator = "#//#")
+{
+    string Line = "";
+    Line += User.UserName + Separator;
+    Line += User.Password + Separator;
+    Line += to_string(User.Permissions);
+    return Line;
+}
+
+// =====================
+// File Functions
+// =====================
+
+void AddDataLineToFile(string FileName, string stDataLine)
+{
+    fstream MyFile;
+    MyFile.open(FileName, ios::out | ios::app);
+    if (MyFile.is_open())
+    {
+        MyFile << stDataLine << endl;
+        MyFile.close();
+    }
+}
+
+vector<stClient> LoadClientsFromFile(string FileName)
+{
+    vector<stClient> vClients;
+    fstream MyFile;
+    MyFile.open(FileName, ios::in);
+
+    if (MyFile.is_open())
+    {
+        string Line;
+        while (getline(MyFile, Line))
+        {
+            if (Line != "")
+                vClients.push_back(ConvertLineToClientRecord(Line));
+        }
+        MyFile.close();
+    }
+    return vClients;
+}
+
+vector<stUser> LoadUsersFromFile(string FileName)
+{
+    vector<stUser> vUsers;
+    fstream MyFile;
+    MyFile.open(FileName, ios::in);
+
+    if (MyFile.is_open())
+    {
+        string Line;
+        while (getline(MyFile, Line))
+        {
+            if (Line != "")
+                vUsers.push_back(ConvertLineToUserRecord(Line));
+        }
+        MyFile.close();
+    }
+    return vUsers;
+}
+
+void SaveClientsToFile(string FileName, vector<stClient>& vClients)
+{
+    fstream MyFile;
+    MyFile.open(FileName, ios::out);
+    if (MyFile.is_open())
+    {
+        for (stClient& C : vClients)
+        {
+            if (!C.MarkForDelete)
+                MyFile << ConvertClientRecordToLine(C) << endl;
+        }
+        MyFile.close();
+    }
+}
+
+void SaveUsersToFile(string FileName, vector<stUser>& vUsers)
+{
+    fstream MyFile;
+    MyFile.open(FileName, ios::out);
+    if (MyFile.is_open())
+    {
+        for (stUser& U : vUsers)
+        {
+            if (!U.MarkForDelete)
+                MyFile << ConvertUserRecordToLine(U) << endl;
+        }
+        MyFile.close();
+    }
+}
+
 int main()
 {
     return 0;
